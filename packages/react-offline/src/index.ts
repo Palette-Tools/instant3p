fe8d4032455e311613960df17e03f9950e557ace
htinney <hunter.tinney@gmail.com>
2025-07-15 15:02:26 -0600

    [chore] initial fork of react

---
+++ packages/react-offline/src/index.ts
@@ -0,0 +1,125 @@
+import {
+  id,
+  tx,
+  lookup,
+  i,
+
+  // types
+  type QueryResponse,
+  type InstantQuery,
+  type InstantQueryResult,
+  type InstantSchema,
+  type InstantObject,
+  type InstantEntity,
+  type InstantSchemaDatabase,
+  type IInstantDatabase,
+  type User,
+  type AuthState,
+  type Query,
+  type Config,
+  type InstaQLParams,
+  type ConnectionStatus,
+
+  // schema types
+  type AttrsDefs,
+  type CardinalityKind,
+  type DataAttrDef,
+  type EntitiesDef,
+  type EntitiesWithLinks,
+  type EntityDef,
+  type InstantGraph,
+  type LinkAttrDef,
+  type LinkDef,
+  type LinksDef,
+  type ResolveAttrs,
+  type ValueTypes,
+  type InstaQLEntity,
+  type InstaQLFields,
+  type InstaQLResult,
+  type InstantUnknownSchema,
+  type InstantSchemaDef,
+  type BackwardsCompatibleSchema,
+  type InstantRules,
+  type UpdateParams,
+  type LinkParams,
+  type ExchangeCodeForTokenParams,
+  type SendMagicCodeParams,
+  type SendMagicCodeResponse,
+  type SignInWithIdTokenParams,
+  type VerifyMagicCodeParams,
+  type VerifyResponse,
+
+  // storage types
+  type FileOpts,
+  type UploadFileResponse,
+  type DeleteFileResponse,
+} from '@instantdb/core';
+
+import InstantReactAbstractDatabase from './InstantReactAbstractDatabase.ts';
+import InstantReactWebDatabase from './InstantReactWebDatabase.ts';
+import { init, init_experimental } from './init.ts';
+import { Cursors } from './Cursors.tsx';
+
+export {
+  id,
+  tx,
+  lookup,
+  init,
+  init_experimental,
+  InstantReactWebDatabase,
+  Cursors,
+  i,
+
+  // internal
+  InstantReactAbstractDatabase,
+
+  // types
+  type Config,
+  type Query,
+  type QueryResponse,
+  type InstantObject,
+  type User,
+  type AuthState,
+  type ConnectionStatus,
+  type InstantQuery,
+  type InstantQueryResult,
+  type InstantSchema,
+  type InstantEntity,
+  type InstantSchemaDatabase,
+  type IInstantDatabase,
+  type InstaQLParams,
+  type InstaQLFields,
+
+  // schema types
+  type AttrsDefs,
+  type CardinalityKind,
+  type DataAttrDef,
+  type EntitiesDef,
+  type EntitiesWithLinks,
+  type EntityDef,
+  type InstantGraph,
+  type LinkAttrDef,
+  type LinkDef,
+  type LinksDef,
+  type ResolveAttrs,
+  type ValueTypes,
+  type InstaQLEntity,
+  type InstaQLResult,
+  type InstantUnknownSchema,
+  type InstantSchemaDef,
+  type BackwardsCompatibleSchema,
+  type InstantRules,
+  type UpdateParams,
+  type LinkParams,
+  type ExchangeCodeForTokenParams,
+  type SendMagicCodeParams,
+  type SendMagicCodeResponse,
+  type SignInWithIdTokenParams,
+  type VerifyMagicCodeParams,
+  type VerifyResponse,
+
+  // storage types
+  type FileOpts,
+  type UploadFileResponse,
+  type DeleteFileResponse,
+};
